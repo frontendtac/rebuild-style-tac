aabf48ef4f7cd7faf00dd24db8da9c8a4fcb7846
frontendtac <frontend.tac@toqueacampainha.com.br>
2016-06-21 08:52:29 -0300

    Add Verifications if element exists

--- js/rebuild-js-tac.js
+++ js/rebuild-js-tac.js
@@ -1,58 +1,44 @@
+/*
+  RJS-TAC V.1.29
+*/
 // Calc freight feature
 jQuery(document).ready(function($) {
 	setTimeout(function() { 
-		/* Remove resultado do cálculo de frete caso o usuário apague o input de cep */
-		$("#txtCep").keyup(function() {
-			if($("#txtCep").val().length ==0) {
-			    $('p.montagem').hide();
-			    $('div.freight-values').hide();
-			    console.log('caiu no if');
-			} else {
-			    $('p.montagem').hide();
-			    $('div.freight-values').hide();
-			}
-		});
-		/* Remove resultado do cálculo de frete caso o usuário apague o input de cep */
+		if (jQuery(".frete").length) {
+			/* Remove resultado do cálculo de frete caso o usuário apague o input de cep */
+			$("#txtCep").keyup(function() {
+				if($("#txtCep").val().length ==0) {
+				    $('p.montagem').hide();
+				    $('div.freight-values').hide();				    
+				} else {
+				    $('p.montagem').hide();
+				    $('div.freight-values').hide();
+				}
+			});
+			/* Remove resultado do cálculo de frete caso o usuário apague o input de cep */
+		}
 	}, 10000);
 });
 // Calc freight feature
 
-// SCRIPT FOR MODAL POPUP WINDOW
-jQuery(document).ready(function(){	
-	jQuery('#popup-container a.close').click(function(){
-			jQuery('#popup-container').fadeOut();
-			jQuery('#active-popup').fadeOut();
-	});
-	
-	var visits = jQuery.cookie('visits') || 0;
-	visits++;
-	
-	jQuery.cookie('visits', visits, { expires: 1, path: '/' });
-		
-	console.debug(jQuery.cookie('visits'));
-		
-	if ( jQuery.cookie('visits') > 1 ) {
-		jQuery('#active-popup').hide();
-		jQuery('#popup-container').hide();
-	} else {
-			var pageHeight = jQuery(document).height();
-			jQuery('<div id="active-popup"></div>').insertBefore('body');
-			jQuery('#active-popup').css("height", pageHeight);
-			jQuery('#popup-container').show();
-	}
+// Toggle List Itens
+jQuery(document).ready(function() {	
+	setTimeout(function() { 
+		if (jQuery(".list-items-policy").length) {
+			$('#toggle-view li').click(function () {
 
-	if (jQuery.cookie('noShowWelcome')) { jQuery('#popup-container').hide(); jQuery('#active-popup').hide(); }
+				var text = $(this).children('div.panel');
+
+				if (text.is(':hidden')) {
+					text.slideDown('200');
+					//$(this).children('span').html('-');		
+				} else {
+					text.slideUp('200');
+					//$(this).children('span').html('+');		
+				}				
+			});
+		}
+	}, 1020);
 });	
-
-jQuery(document).mouseup(function(e){
-	var container = jQuery('#popup-container');
-	
-	if( !container.is(e.target)&& container.has(e.target).length === 0)
-	{
-		container.fadeOut();
-		jQuery('#active-popup').fadeOut();
-	}
-
-});
-// SCRIPT FOR MODAL POPUP WINDOW
+// Toggle List Itens
 
